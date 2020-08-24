d6d6a8a12001e1deda5b572b138985b118dd433f
Frank Kopp <frank@familie-kopp.de>
2020-08-25 00:30:53 +0200

    Added Google Benchmark and added some bench tests

--- testbench/RunBench.cpp
+++ testbench/RunBench.cpp
@@ -25,4 +25,8 @@
 
 #include <benchmark/benchmark.h>
 
-BENCHMARK_MAIN();
+int main(int argc, char* argv[]) {
+  ::benchmark::Initialize(&argc, argv);
+  if (::benchmark::ReportUnrecognizedArguments(argc, argv)) return 1;
+  ::benchmark::RunSpecifiedBenchmarks();
+}
