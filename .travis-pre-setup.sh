dae85262e7e2589a393d86edcfdb3d5d11baa17b
Frank Kopp <frank@familie-kopp.de>
2020-08-25 23:39:05 +0200

    Travis CI Tests

--- .travis-pre-setup.sh
+++ .travis-pre-setup.sh
@@ -4,3 +4,5 @@
 curl -sSL https://cmake.org/files/v3.18/cmake-3.18.2-Linux-x86_64.sh -o install-cmake.sh
 chmod +x install-cmake.sh
 sudo ./install-cmake.sh --prefix=/usr/local --skip-license
+ls -l /usr/local
+ls -l /usr/local/bin
