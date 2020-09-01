e79d4508c84c1f895b4bbefd5ba02b966eb3c452
Frank Kopp <frank@familie-kopp.de>
2020-09-01 14:02:05 +0200

    Cosmetics

--- test/PlaygroundTests.cpp
+++ test/PlaygroundTests.cpp
@@ -28,6 +28,67 @@
 #include <gtest/gtest.h>
 using testing::Eq;
 
-TEST(Playground, format) {
-  fmt::print("{:L}", 123456789);
+#include <iostream>
+#include <string>
+#include <vector>
+
+TEST(Playground, emplace) {
+
+  struct A {
+    std::string s;
+    A(std::string str) : s(std::move(str)) {
+      std::cout << " constructed\n";
+    }
+    A(const A& o) : s(o.s) {
+      std::cout << " copy constructed\n";
+    }
+    A(A&& o) : s(std::move(o.s)) {
+      std::cout << " move constructed\n";
+    }
+    A& operator=(const A& other) {
+      s = other.s;
+      std::cout << " copy assigned\n";
+      return *this;
+    }
+    A& operator=(A&& other) {
+      s = std::move(other.s);
+      std::cout << " move assigned\n";
+      return *this;
+    }
+    void * operator new(size_t size) {
+      std::cout<< "new (allocated size=: " << size << ")" << std::endl;
+//      void * p = ::new A("");
+      void * p = malloc(size);
+      return p;
+    }
+    void operator delete(void * p) {
+      std::cout<< "delete" << std::endl;
+      free(p);
+    }
+  };
+
+  std::vector<A> container;
+  // reserve enough place so vector does not have to resize
+  container.reserve(10);
+  std::cout << "construct 2 times A:\n";
+  A two{"two"};
+  A three{"three"};
+
+  std::cout << "emplace:\n";
+  container.emplace(container.end(), "one");
+
+  std::cout << "emplace with A&:\n";
+  container.emplace(container.end(), two);
+
+  std::cout << "emplace with A&&:\n";
+  container.emplace(container.end(), std::move(three));
+
+  std::cout << "emplace with new A():\n";
+  A* four = new A("four");
+  container.emplace(container.end(), *four);
+
+  std::cout << "content:\n";
+  for (const auto& obj : container)
+    std::cout << ' ' << obj.s;
+  std::cout << '\n';
 }
